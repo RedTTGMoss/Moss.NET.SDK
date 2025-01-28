48c3f9ea13cd565438fee73cd9a2b9c77d5ba361
Chris Anders <filmee24@gmail.com>
2025-01-28 17:56:19 +0100

    feat: Add template

---
+++ src/Moss.Net.Sdk.Templates/content/plugin/SampleExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Extism;
+using Moss.NET.Sdk;
+using Moss.NET.Sdk.FFI;
+
+namespace SamplePlugin;
+
+public class SampleExtension : MossExtension
+{
+    [UnmanagedCallersOnly(EntryPoint = "moss_extension_register")]
+    public static ulong Register()
+    {
+        Init<SampleExtension>();
+
+        return 0;
+    }
+
+    public static void Main()
+    {
+         Pdk.Log(LogLevel.Info, "extension started");
+    }
+
+    public override ExtensionInfo Register(MossState state)
+    {
+        Pdk.Log(LogLevel.Info, "registered sample extension");
+
+        Config.Set("theme", "dark");
+        Theme.Apply(new DarkTheme());
+
+        return new ExtensionInfo([]);
+    }
+
+    public override void Unregister()
+    {
+        Pdk.Log(LogLevel.Info, "unregistered sample extension");
+    }
+}
