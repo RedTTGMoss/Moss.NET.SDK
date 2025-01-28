4881a1c146f540bdda559df7b4bb3a49b36d63a0
Chris Anders <filmee24@gmail.com>
2025-01-28 12:09:14 +0000

    feat: Add new api functions

--- src/Moss.NET.Sdk/FFI/Functions.cs
+++ src/Moss.NET.Sdk/FFI/Functions.cs
@@ -2,12 +2,18 @@
 
 namespace Moss.NET.Sdk.FFI;
 
+//refers to https://redttg.gitbook.io/moss/extensions/host-functions
 internal static class Functions
 {
     private const string DLL = "extism";
 
     [DllImport(DLL, EntryPoint = "moss_gui_register_context_menu")]
-    public static extern void RegisterMenu(ulong ptr);
+    public static extern void RegisterContextMenu(ulong ptr);
+
+
+    [DllImport(DLL, EntryPoint = "moss_gui_open_context_menu")]
+    public static extern void OpenContextMenu(ulong keyPtr, int x, int y);
+
 
     [DllImport(DLL, EntryPoint = "moss_em_config_get")]
     public static extern ulong GetConfig(ulong ptr);
@@ -20,4 +26,19 @@ internal static class Functions
 
     [DllImport(DLL, EntryPoint = "moss_defaults_set_text_color")]
     public static extern void SetDefaultTextColor(ulong keyPtr, ulong textColorPtr);
+
+
+    [DllImport(DLL, EntryPoint = "moss_defaults_get_text_color")]
+    public static extern void GetDefaultTextColor(ulong keyPtr);
+
+    [DllImport(DLL, EntryPoint = "moss_defaults_get")]
+    public static extern void GetDefaultValue(ulong keyPtr);
+
+
+    [DllImport(DLL, EntryPoint = "_moss_defaults_set")]
+    public static extern void SetDefaultValue(ulong configSetPtr);
+
+
+    [DllImport(DLL, EntryPoint = "moss_gui_invert_icon")]
+    public static extern void InvertIcon(ulong keyPtr, ulong resultKeyPtr);
 }
