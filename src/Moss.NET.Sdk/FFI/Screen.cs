2a69286dd0f566003bd2d03f4b7dcfb3cf458a60
Chris <filmee24@gmail.com>
2025-01-28 14:10:59 +0100

    Update Screen.cs

--- src/Moss.NET.Sdk/FFI/Screen.cs
+++ src/Moss.NET.Sdk/FFI/Screen.cs
@@ -1,9 +1,9 @@
 /*
 Screen & ChildScreen: - These are considered UI rendering objects and will have respective context handling
 - key: str - An identifier for the screen
-- screen_pre_loop: str - An extension function for the before loop call
+- screen_pre_loop: str - An extension function for the before loop call (optional)
 - screen_loop: str - An extension function for the loop call
-- screen_post_loop: str - An extension function for the after loop call
-- event_hook: str - An extension function which will receive API events, must accept a single argument name: String
+- screen_post_loop: str - An extension function for the after loop call (optional)
+- event_hook: str - An extension function which will receive API events, must accept a single argument name: String (otpional)
 */
-public record Screen(string key, string screen_pre_loop, string screen_loop, string screen_post_loop, string event_hook);
+public record Screen(string key, string screen_loop, string? screen_pre_loop = null, string? screen_post_loop = null, string? event_hook = null);
