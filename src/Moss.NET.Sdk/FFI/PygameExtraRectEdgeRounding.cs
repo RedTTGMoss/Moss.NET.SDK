ce18482098674d56714fa5f97b6989a4833fabd3
Chris Anders <filmee24@gmail.com>
2025-01-28 12:33:22 +0000

    refactor: Cleanup

--- src/Moss.NET.Sdk/FFI/PygameExtraRectEdgeRounding.cs
+++ src/Moss.NET.Sdk/FFI/PygameExtraRectEdgeRounding.cs
@@ -1,2 +1,3 @@
+//ToDo: make all edgerounding properties optional
 public record PygameExtraRectEdgeRounding(int edge_rounding, int edge_rounding_topright, int edge_rounding_topleft,
     int edge_rounding_bottomright, int edge_rounding_bottomleft);
