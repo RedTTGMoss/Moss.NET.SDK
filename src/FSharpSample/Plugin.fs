6011ca9643333b513f2c158f01cc4af1233b740c
Chris Anders <filmee24@gmail.com>
2025-03-08 22:30:23 +0100

    feat: Add F# Sample

---
+++ src/FSharpSample/Plugin.fs
@@ -0,0 +1,21 @@
+﻿module SamplePlugin.Plugin
+
+open System.Runtime.InteropServices
+open Moss.NET.Sdk
+
+type SampleExtension() =
+        inherit MossExtension()
+
+        static let logger: LoggerInstance = Log.GetLogger<SampleExtension>()
+
+        [<UnmanagedCallersOnly(EntryPoint = "moss_extension_register")>]
+        static member Register() : uint64 =
+            SampleExtension.Init<SampleExtension>()
+            0UL
+
+        static member Main() = ()
+
+        override this.Register(state: MossState)  =
+            logger.Info("Hello world from f#")
+
+        override this.Unregister() = ()
