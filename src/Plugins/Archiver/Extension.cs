41feaf133fef20476216531a0c041c69349054b5
Chris Anders <filmee24@gmail.com>
2025-03-09 20:25:32 +0100

    refactor: Cleanup

---
+++ src/Plugins/Archiver/Extension.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+using Extism;
+using Moss.NET.Sdk;
+
+namespace SamplePlugin;
+
+public class Extension : MossExtension
+{
+    [UnmanagedCallersOnly(EntryPoint = "archiver_extension_register")]
+    public static ulong Register()
+    {
+        Init<Extension>();
+
+        return 0;
+    }
+
+    public static void Main()
+    {
+    }
+
+    public override void Register(MossState state)
+    {
+        Pdk.Log(LogLevel.Info, "Hello world form sample plugin");
+    }
+
+    public override void Unregister()
+    {
+    }
+}
