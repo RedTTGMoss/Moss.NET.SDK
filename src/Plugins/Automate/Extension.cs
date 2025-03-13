fd684d64527aac867e645cd1046f751b90708456
Chris Anders <filmee24@gmail.com>
2025-03-13 19:44:12 +0100

    feat: Add skeleton for automate extension

---
+++ src/Plugins/Automate/Extension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Extism;
+using Moss.NET.Sdk;
+using NiL.JS.Core;
+
+namespace Automate;
+
+public class Extension : MossExtension
+{
+    [ModuleInitializer]
+    public static void ModInit()
+    {
+        Init<Extension>();
+    }
+
+    public static void Main()
+    {
+    }
+
+    public override void Register(MossState state)
+    {
+         var context = new Context();
+         context.DefineVariable("log").Assign(JSValue.Marshal(new Action<string>(x => Pdk.Log(LogLevel.Info, x))));
+
+         context.Eval("log('hello from js')");
+    }
+
+    public override void Unregister()
+    {
+    }
+}
