6803d499cc2d722668bdffcf696cabe0736eb5f7
Chris Anders <filmee24@gmail.com>
2025-04-04 20:27:15 +0200

    feat: Add RestTemplate

---
+++ src/Moss.NET.Sdk/RestTemplate.cs
@@ -0,0 +1,27 @@
+﻿using Extism;
+using PipelineNet.Middleware;
+using PipelineNet.MiddlewareResolver;
+using PipelineNet.Pipelines;
+
+public class RestTemplate
+{
+    private readonly Pipeline<HttpRequest> _pipeline = new(new ActivatorMiddlewareResolver());
+
+    public void Use<T>()
+        where T : IMiddleware<HttpRequest, HttpResponse>
+    {
+        _pipeline.Add(typeof(T));
+    }
+
+    public HttpResponse Execute(HttpRequest request)
+    {
+        _pipeline.Execute(request);
+
+        return Pdk.SendRequest(request);
+    }
+
+    public HttpResponse Exchange(string url)
+    {
+        return Execute(new HttpRequest(url));
+    }
+}
