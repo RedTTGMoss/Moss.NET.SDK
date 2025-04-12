f0b3240ca91fc5f35bb1c56d3114e8c903c61227
Chris Anders <filmee24@gmail.com>
2025-04-12 12:40:13 +0200

    refactor(totletheyn): Add newspaper abstraction

---
+++ src/Plugins/Totletheyn/Jobs/FontSize.cs
@@ -0,0 +1,13 @@
+﻿namespace Totletheyn.Jobs;
+
+public enum FontSize
+{
+    Headline = 75,
+    HeaderInfo = 15,
+    SidebarH2 = 29,
+    MainContentH2 = 29,
+    ExtraContentH2 = 29,
+    ArticleH3 = 24,
+    ArticleP = 16,
+    Footer = 10
+}
